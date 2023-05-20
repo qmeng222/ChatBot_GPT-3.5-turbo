c2ff2b3465e7bf63cd402cde58aaee51bc1d112f
Qingying Meng <qmeng222@gmail.com>
2023-05-19 22:59:19 -0700

    added optional chatbot personalities

--- ReadMe.md
+++ ReadMe.md
@@ -2,3 +2,4 @@
 
 1. install OpenAI: `pip install openai`
 2. install the Python package called python-dotenv: `pip install python_dotenv`
+3. run the ChatBot with a personality setting: `python chatbot.py --personality "silly and goofy"`
--- chatbot.py
+++ chatbot.py
@@ -1,30 +1,42 @@
 import openai
 from dotenv import dotenv_values
-
+import argparse
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-messages = []
 
-while True:
-  try:
-    # user's input:
-    user_input = input("User: ")
-    messages.append({"role": "user", "content": user_input})
+def main():
+  parser = argparse.ArgumentParser(description="Simple command line chatbot")
+  parser.add_argument("--personality", type=str, help="A brief summary of the chatbot's personality or its main function", default="friendly and helpful")
+  args = parser.parse_args()
 
-    # assistant's response:
-    res = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": user_input}],
-        max_tokens=200,
-    )
-    messages.append(res.choices[0].message.to_dict())
-    print("Assistant: ", res.choices[0].message.content)
+  # print(args.personality)  # "silly and goofy"
+  initial_prompt = f"You are a conversational chatbot. Your personality is: {args.personality}."
+  messages = [{"role": "system", "content": "initial_prompt"}]
 
-    # # complete message history:
-    # print("All messages: ", messages)
+  # list of messages:
+  while True:
+    try:
+      # user's input:
+      user_input = input("User: ")
+      messages.append({"role": "user", "content": user_input})
 
-  except KeyboardInterrupt: # Ctrl + C
-    print("Exiting ...")
-    break
+      # assistant's response:
+      res = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[{"role": "user", "content": user_input}],
+          max_tokens=200,
+      )
+      messages.append(res.choices[0].message.to_dict())
+      print("Assistant: ", res.choices[0].message.content)
+
+      # # complete message history:
+      # print("All messages: ", messages)
+
+    except KeyboardInterrupt: # Ctrl + C
+      print("Exiting ...")
+      break
+
+if __name__ == "__main__":
+  main()
